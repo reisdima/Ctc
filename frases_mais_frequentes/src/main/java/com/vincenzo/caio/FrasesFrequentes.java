35229a470b1a00037eb0a1a7c59c0c28caacaf00
Caio <caio.vincenzo@estudante.ufjf.br>
2021-10-10 21:42:26 -0300

    Ordena por frequencia e cria arquivo de saida

--- frases_mais_frequentes/src/main/java/com/vincenzo/caio/FrasesFrequentes.java
+++ frases_mais_frequentes/src/main/java/com/vincenzo/caio/FrasesFrequentes.java
@@ -6,18 +6,21 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class FrasesFrequentes {
     private Scanner scannerLeitura;
-    private Scanner scannerEscrita;
     private Map<Integer, String> frasesUnicas;
     private Map<Integer, Integer> frasesContador;
+    private Map<Integer, Integer> frasesContadorOrdenado;
 
-    private final static int LINHAS = 5000;
+    private final static int LINHAS = 50000;
 
     public FrasesFrequentes() {
         frasesUnicas = new HashMap<>();
@@ -38,11 +41,11 @@ public class FrasesFrequentes {
                     frasesContador.merge(hashCode, 1, (contador, um) -> contador + 1);
                 }
             }
-            System.out.println("Frases Únicas: ");
-            frasesUnicas.forEach((key, value) -> System.out.println(key + ":" + value));
-            System.out.println('\n');
-            System.out.println("Frases contador: ");
-            frasesContador.forEach((key, value) -> System.out.println(key + ":" + value));
+            frasesContadorOrdenado = frasesContador.entrySet().stream()
+                    .sorted(Collections.reverseOrder(Map.Entry.comparingByValue())).limit(50000).collect(Collectors
+                            .toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+
+            salvarResultado();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -60,7 +63,7 @@ public class FrasesFrequentes {
             frases.add(frase);
         }
         for (int i = 0; i < LINHAS; i++) {
-            int frasesNaLinha = (int) (Math.random() * 50 + 1);
+            int frasesNaLinha = (int) (Math.random() * 500 + 1);
             for (int j = 0; j < frasesNaLinha; j++) {
                 int random = (int) (Math.random() * frases.size());
                 String frase = frases.get(random);
@@ -78,5 +81,19 @@ public class FrasesFrequentes {
         File file = new File("./resources/frasesFrequentes.txt");
         inputStream = new FileInputStream(file);
         return inputStream;
+    }
+
+    public void salvarResultado() throws IOException {
+        FileOutputStream outputStream = new FileOutputStream("./resources/frasesResultado.txt");
+        frasesContadorOrdenado.forEach((key, value) -> {
+            String frase = value + " - " + frasesUnicas.get(key) + '\n';
+            byte[] strToBytes = frase.getBytes();
+            try {
+                outputStream.write(strToBytes);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        outputStream.close();
     }
 }
