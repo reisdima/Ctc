7abc6a4f07d98c51cd8a1ae29455aa714fb116ba
Caio <caio.vincenzo@estudante.ufjf.br>
2021-10-10 21:15:14 -0300

    Adiciona função para gerar arquivo com frases

--- frases_mais_frequentes/src/main/java/com/vincenzo/caio/App.java
+++ frases_mais_frequentes/src/main/java/com/vincenzo/caio/App.java
@@ -1,11 +1,17 @@
 package com.vincenzo.caio;
 
+import java.io.IOException;
+
 public final class App {
     private App() {
     }
 
     public static void main(String[] args) {
         FrasesFrequentes frasesFrequentes = new FrasesFrequentes();
-        frasesFrequentes.executar();
+        try {
+            frasesFrequentes.executar();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- frases_mais_frequentes/src/main/java/com/vincenzo/caio/FrasesFrequentes.java
+++ frases_mais_frequentes/src/main/java/com/vincenzo/caio/FrasesFrequentes.java
@@ -3,40 +3,41 @@ package com.vincenzo.caio;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class FrasesFrequentes {
-    Scanner scannerLeitura;
-    Scanner scannerEscrita;
-    Map<Integer, String> frasesUnicas;
-    Map<Integer, Integer> frasesContador;
+    private Scanner scannerLeitura;
+    private Scanner scannerEscrita;
+    private Map<Integer, String> frasesUnicas;
+    private Map<Integer, Integer> frasesContador;
+
+    private final static int LINHAS = 5000;
 
     public FrasesFrequentes() {
         frasesUnicas = new HashMap<>();
         frasesContador = new HashMap<>();
     }
 
-    public void executar() {
-        // criarArquivo();
+    public void executar() throws IOException {
+        criarArquivo();
         try {
             FileInputStream inputStream = lerArquivo();
             scannerLeitura = new Scanner(inputStream, "UTF-8");
-            // while (scannerLeitura.hasNextLine()) {
-            String linha = scannerLeitura.nextLine();
-            System.out.println("Linha: ");
-            System.out.println(linha);
-            String[] frases = linha.split("\\|");
-            for (String frase : frases) {
-                System.out.println("Frase: ");
-                System.out.println(frase);
-                // String hashCode = geraHashCode(frase);
-                int hashCode = frase.hashCode();
-                frasesUnicas.put(hashCode, frase);
-                frasesContador.merge(hashCode, 1, (contador, um) -> contador + 1);
+            while (scannerLeitura.hasNextLine()) {
+                String linha = scannerLeitura.nextLine();
+                String[] frases = linha.split("\\|");
+                for (String frase : frases) {
+                    int hashCode = frase.hashCode();
+                    frasesUnicas.put(hashCode, frase);
+                    frasesContador.merge(hashCode, 1, (contador, um) -> contador + 1);
+                }
             }
-            // }
             System.out.println("Frases Únicas: ");
             frasesUnicas.forEach((key, value) -> System.out.println(key + ":" + value));
             System.out.println('\n');
@@ -48,12 +49,28 @@ public class FrasesFrequentes {
 
     }
 
-    public String geraHashCode(String frase) {
-        return "";
-    }
+    public void criarArquivo() throws IOException {
+        FileOutputStream outputStream = new FileOutputStream("./resources/frasesFrequentes.txt");
+        File file = new File("./resources/frases.txt");
+        FileInputStream inputStream = new FileInputStream(file);
+        scannerLeitura = new Scanner(inputStream, "UTF-8");
+        List<String> frases = new ArrayList<>();
+        while (scannerLeitura.hasNextLine()) {
+            String frase = scannerLeitura.nextLine();
+            frases.add(frase);
+        }
+        for (int i = 0; i < LINHAS; i++) {
+            int frasesNaLinha = (int) (Math.random() * 50 + 1);
+            for (int j = 0; j < frasesNaLinha; j++) {
+                int random = (int) (Math.random() * frases.size());
+                String frase = frases.get(random);
+                byte[] strToBytes = (frase.concat("|")).getBytes();
+                outputStream.write(strToBytes);
+            }
+            outputStream.write('\n');
+        }
 
-    public void criarArquivo() {
-
+        outputStream.close();
     }
 
     public FileInputStream lerArquivo() throws FileNotFoundException {
